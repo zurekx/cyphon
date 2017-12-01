d8850dae44f6af9f549aee816b98535b2688cbea
lhadjchikh <leila.hadj-chikh@dunbarsecured.com>
2017-12-01 10:51:01 -0500

    fix docstring

--- cyphon/responder/actions/serializers.py
+++ cyphon/responder/actions/serializers.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Cyphon Engine. If not, see <http://www.gnu.org/licenses/>.
 """
-Serializers for Contexts.
+Serializers for Actions.
 """
 
 # third party
