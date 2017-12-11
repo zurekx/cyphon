6113d50189278e67e739bc221be9f3e7d25eb8e8
lhadjchikh <leila.hadj-chikh@dunbarsecured.com>
2017-12-11 15:38:08 -0500

    fix module docstring

--- cyphon/platforms/virustotal/handlers.py
+++ cyphon/platforms/virustotal/handlers.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Cyphon Engine. If not, see <http://www.gnu.org/licenses/>.
 """
-Defines classes for JIRA actions.
+Defines classes for VirusTotal handlers.
 """
 
 # standard library
