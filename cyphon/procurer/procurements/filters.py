379d5bfc07fdecfa9b7266013ecb36ec7053a991
lhadjchikh <leila.hadj-chikh@dunbarsecured.com>
2017-12-11 15:49:26 -0500

    fix module docstring

--- cyphon/procurer/procurements/filters.py
+++ cyphon/procurer/procurements/filters.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Cyphon Engine. If not, see <http://www.gnu.org/licenses/>.
 """
-Defines AlertFilter, CommentFilter, and CommentFilterBackend classes.
+Defines filters for Procurements.
 """
 
 # third party
