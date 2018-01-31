1ae3536ccabac82684cf8277a3083de09d5d0349
lhadjchikh <leila.hadj-chikh@dunbarsecured.com>
2018-01-31 16:49:49 -0500

    add VirusTotal tests

--- cyphon/platforms/virustotal/tests/test_handlers.py
+++ cyphon/platforms/virustotal/tests/test_handlers.py
@@ -16,103 +16,152 @@
 # along with Cyphon Engine. If not, see <http://www.gnu.org/licenses/>.
 
 # standard library
-# try:
-#     from unittest.mock import Mock, patch
-# except ImportError:
-#     from mock import Mock, patch
-
-# standard library
 from unittest import skipUnless
 
 # third party
 from celery.contrib.testing.worker import start_worker
-from django.test import SimpleTestCase, TestCase, TransactionTestCase
-from rest_framework.test import APISimpleTestCase
+from django.test import TestCase, TransactionTestCase
 
 # local
 from ambassador.passports.models import Passport
 from cyphon.celeryapp import app
-from platforms.virustotal.handlers import UrlReport
+from platforms.virustotal.handlers import UrlReport, UrlScan
 from procurer.requisitions.models import Requisition
-from procurer.supplychains.models import SupplyChain
 from procurer.supplyorders.models import SupplyOrder
 from tests.api_tests import PassportMixin
 from tests.fixture_manager import get_fixtures
 from .settings import VIRUSTOTAL_SETTINGS, VIRUSTOTAL_TESTS_ENABLED
 
 
-# class VirusTotalTestCase(TestCase):
-#     """
-#     Base class for VirusTotal tests.
-#     """
+class VirusTotalSupplyChainTestCase(TransactionTestCase, PassportMixin):
+    """
+    Integration test for a SupplyOrder for VirusTotal.
+    """
 
-#     fixtures = get_fixtures(['virustotal'])
+    allow_database_queries = True
+
+    fixtures = get_fixtures(['supplyorders', 'quartermasters'])
+
+    @classmethod
+    def setUpClass(cls):
+        super(VirusTotalSupplyChainTestCase, cls).setUpClass()
+
+        # Start up celery worker
+        cls.celery_worker = start_worker(app)
+        cls.celery_worker.__enter__()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Close worker
+        cls.celery_worker.__exit__(None, None, None)
+        super(VirusTotalSupplyChainTestCase, cls).tearDownClass()
+
+    def setUp(self):
+        super(VirusTotalSupplyChainTestCase, self).setUp()
+
+        private_passport = Passport.objects.get_by_natural_key('VirusTotal Private')
+        self._update_passport(private_passport, VIRUSTOTAL_SETTINGS)
+
+        public_passport = Passport.objects.get_by_natural_key('VirusTotal Public')
+        self._update_passport(public_passport, VIRUSTOTAL_SETTINGS)
+
+    # @skipUnless(VIRUSTOTAL_TESTS_ENABLED, 'VirusTotal API tests disabled')
+    # def test_process_request(self):
+    #     """
+
+    #     """
+    #     self.maxDiff = None
+    #     supply_order = SupplyOrder.objects.get(pk=1)
+    #     supply_order = supply_order.process()
+
+    #     actual_manifests = ([manifest.data for manifest in supply_order.manifests.all()])
+    #     expected_manifests = [
+    #         {
+    #             'url': 'http://dunbararmored.com'
+    #         }, {
+    #             'resource': 'http://dunbararmored.com/'
+    #         }
+    #     ]
+    #     self.assertEqual(actual_manifests, expected_manifests)
+
+    #     expected_result = {
+    #         'total': 67,
+    #         '_platform': 'virustotal',
+    #         '_distillery': 4,
+    #         'url': 'http://dunbararmored.com/',
+    #         'resource': 'http://dunbararmored.com/',
+    #         'positives': 0,
+    #         'filescan_id': None,
+    #     }
+    #     actual_result = supply_order.result
+    #     for key in ['_id', '_saved_date', 'permalink', 'scan_date', 'scan_id']:
+    #         actual_result.pop(key)
+    #     self.assertEqual(actual_result, expected_result)
 
 
-# class UrlScanSupplyChainTestCase(TransactionTestCase, PassportMixin):
-#     """
+class VirusTotalTestCase(TestCase, PassportMixin):
+    """
+    Base class for VirusTotal tests.
+    """
 
-#     """
+    fixtures = get_fixtures(['requisitions', 'quartermasters'])
 
-#     allow_database_queries = True
+    def setUp(self):
+        super(VirusTotalTestCase, self).setUp()
 
-#     fixtures = get_fixtures(['supplyorders', 'quartermasters'])
+        private_passport = Passport.objects.get_by_natural_key('VirusTotal Private')
+        self._update_passport(private_passport, VIRUSTOTAL_SETTINGS)
 
-#     @classmethod
-#     def setUpClass(cls):
-#         super(UrlScanSupplyChainTestCase, cls).setUpClass()
+        public_passport = Passport.objects.get_by_natural_key('VirusTotal Public')
+        self._update_passport(public_passport, VIRUSTOTAL_SETTINGS)
 
-#         # Start up celery worker
-#         cls.celery_worker = start_worker(app)
-#         cls.celery_worker.__enter__()
+    @skipUnless(VIRUSTOTAL_TESTS_ENABLED, 'VirusTotal API tests disabled')
+    def test_url_report(self):
+        """
 
-#     @classmethod
-#     def tearDownClass(cls):
-#         # Close worker
-#         cls.celery_worker.__exit__(None, None, None)
-#         super(UrlScanSupplyChainTestCase, cls).tearDownClass()
+        """
+        endpoint = Requisition.objects.get_by_natural_key('virustotal',
+                                                          'UrlReport')
+        handler = UrlReport(endpoint)
+        url = 'dunbararmored.com'
 
-#     def setUp(self):
-#         """
+        cargo = handler.process_request({'resource': url})
 
-#         """
-#         super(UrlScanSupplyChainTestCase, self).setUp()
-#         passport = Passport.objects.get_by_natural_key('VirusTotal Public')
-#         self._update_passport(passport, VIRUSTOTAL_SETTINGS)
+        expected_data = 0
+        actual_data = cargo.data['positives']
+        self.assertEqual(actual_data, expected_data)
 
-#     @skipUnless(VIRUSTOTAL_TESTS_ENABLED, 'VirusTotal API tests disabled')
-#     def test_process_request(self):
-#         """
+        expected_code = 1
+        actual_code = cargo.status_code
+        self.assertEqual(actual_code, expected_code)
 
-#         """
-#         supply_chain = SupplyChain.objects.get_by_natural_key('VirusTotal URL Scan & Report')
-#         supply_order = SupplyOrder.objects.get(pk=1)
-#         # result = supply_order.process()
-#         result = supply_chain.start(supply_order=supply_order)
-#         # print([(manifest.data, manifest.status_code, manifest.response_msg) for manifest in supply_order.manifests.all()])
-#         print(result)
-#         supply_order = SupplyOrder.objects.get(pk=1)
-#         # [({'url': 'http://example.com'}, '403', 'Forbidden'), ({'resource': None}, '403', 'Forbidden')]
+        expected_notes = 'Scan finished, scan information embedded in this object'
+        actual_notes = cargo.notes
+        self.assertEqual(actual_notes, expected_notes)
 
+    # @skipUnless(VIRUSTOTAL_TESTS_ENABLED, 'VirusTotal API tests disabled')
+    # def test_url_scan(self):
+    #     """
 
-# class UrlReportTestCase(VirusTotalTestCase):
-#     """
-#     Tests the UrlReport class.
-#     """
+    #     """
+    #     endpoint = Requisition.objects.get_by_natural_key('virustotal',
+    #                                                       'UrlReport')
+    #     handler = UrlScan(endpoint)
+    #     url = 'dunbararmored.com'
 
-#     def test_process_request(self):
-#         """
+    #     cargo = handler.process_request({'resource': url})
+    #     print(cargo.data)
+    #     expected_data = 0
+    #     actual_data = cargo.data['positives']
+    #     self.assertEqual(actual_data, expected_data)
 
-#         """
-#         endpoint = Requisition.objects.get_by_natural_key('virustotal',
-#                                                           'UrlReport')
-#         handler = UrlReport(endpoint)
-#         url = 'dunbafhjfgfrahfsdhrdfdmored.com'
+    #     expected_code = 1
+    #     actual_code = cargo.status_code
+    #     self.assertEqual(actual_code, expected_code)
 
-#         cargo = handler.process_request({'resource': url})
-#         print('data', cargo.data)
-#         print('code', cargo.status_code)
-#         print('notes', cargo.notes)
+    #     expected_notes = 'Scan finished, scan information embedded in this object'
+    #     actual_notes = cargo.notes
+    #     self.assertEqual(actual_notes, expected_notes)
 
 
 # class UrlScanSupplyChainTestCase(VirusTotalTestCase):
